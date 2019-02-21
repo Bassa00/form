401bf89938a19f1a6771213e3bbd39a5f9a40674
brianttocs <brianttocs@gmail.com>
2019-02-21 08:18:01 +0100

    initiate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='App-Aside'>
+          
+        </div>
+        
+        <div className='App-Form'>
+          <div>
+            <a href='./' className='PageSwitcher-Item'>Sign In</a>
+            <a href='./' className='PageSwitcher-Item PageSwitcher-Item-Active'>Sign Up</a>
+          </div>
+
+          <div className='FormTitle'>
+            <a href='./' className='FormTitle-Link'>Sign In</a> or <a href='./' className='FormTitle-Link FormTitle-Link-Active'>Sign Up</a>
+          </div>
+
+          <div className='FormCenter'>
+            <form className='FormFields' onSubmit={this.handleSubmit}>
+              <div className='FormField'>
+                <label className='FormFiels-Label' htmlFor='name'>FULL NAME </label>
+                <input type='text' id='name' className='FormField-Input' placeholder='Enter your full name..' name='name' />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
